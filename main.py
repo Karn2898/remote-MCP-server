420c59fba7bfec69a606fefb28fa00302a842da4
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-08-16 01:21:08 +0530

    fixes

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ except ImportError:
 import random
 import json
 
-mcp=FastMCP("simple calculator server")
+mcp = FastMCP("simple calculator server")
 
 @mcp.tool()
-def add(a:int , b: int):
+def add(a: int, b: int):
     """Add two numbers"""
     return a + b
+
 @mcp.tool()
-def random_number(min:int=1, max:int=100):
+def random_number(min: int = 1, max: int = 100):
     """Generate a random number between min and max"""
     return random.randint(min, max)
+
 @mcp.resource("info://server")
 def server_info():
-    info={
-        "name":"calculator",
-        "version":"1.0.0",
-        "description":"A simple calculator server that can add numbers and generate random numbers.",
-        "tools":["add","random_number"],
-        "author":"Tamaghna "
-        
+    info = {
+        "name": "calculator",
+        "version": "1.0.0",
+        "description": "A simple calculator server that can add numbers and generate random numbers.",
+        "tools": ["add", "random_number"],
+        "author": "Tamaghna"
     }
     return json.dumps(info, indent=2)
 
+
 if __name__ == "__main__":
     mcp.run(transport="http", host="0.0.0.0", port=8000)
-    
